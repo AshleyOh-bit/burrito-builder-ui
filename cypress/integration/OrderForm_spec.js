be30f2c85f0d4602622fbd1ba7b53a8d80290eec
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-10 11:05:02 -0600

    Add POST intercept for form tests with basic form tests

--- cypress/integration/OrderForm_spec.js
+++ cypress/integration/OrderForm_spec.js
@@ -36,15 +36,32 @@ describe('App user flows', () => {
     })
     cy.visit('http://localhost:3000/')
   })
+
   it("Should display page title", () => {
     cy.get("h1")
       .contains("Burrito Builder")
   })
 
-  it("Should display the information retrieved from the api on a card", () => {
-    cy.get("[data-cy=orderCard]")
-      .should("have.length", "1")
+  it("Should allow the user to type in their name", () => {
+    cy.get("input")
+      .type("Brandon")
+      .should("have.value", "Brandon")
   })
+
+  it("Should allow the user to choose ingredients and show those ingredient in the order preview" , () => {
+    cy.get("#0")
+      .click()
+    cy.get("p")
+      .contains("Order: beans")
+  })
+
+  it("Should display a disabled submit button if there is no user name but there is an ingredient", () => {
+    cy.get("#0")
+      .click()
+    cy.get("[data-cy=submit]")
+      .should("be.disabled")
+  })
+
 
   
 })
