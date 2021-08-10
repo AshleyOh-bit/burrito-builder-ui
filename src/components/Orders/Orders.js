de38d855b1f10cc0c316b603d36ad4b68b6652b4
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-10 11:28:26 -0600

    Test for specific information returned from post

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -4,9 +4,9 @@ import './Orders.css';
 const Orders = props => {
   const orderEls = props.orders.map(order => {
     return (
-      <div className="order" data-cy="orderCard" key={order.id} id={order.id}>
+      <div className="order" data-cy="orderCard" key={order.id} id={order.name}>
         <h3 data-cy="orderName">{order.name}</h3>
-        <ul className="ingredient-list">
+        <ul className="ingredient-list" data-cy="ingredient-list">
           {order.ingredients.map((ingredient, index) => {
             return <li key={index} data-cy="ingredients">{ingredient}</li>
           })}
